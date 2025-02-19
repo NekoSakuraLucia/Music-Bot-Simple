330c01d56a84b08857ab3585eae3ad042580856e
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-19 17:12:30 +0700

    เพิ่มส่วนเริ่มต้นสำหรับของ shoukaku

--- MusicBot.js
+++ MusicBot.js
@@ -1,8 +1,18 @@
 const { Client, GatewayIntentBits, Events } = require("discord.js");
+const { Shoukaku, Connectors } = require("shoukaku");
 require("dotenv").config();
+
+const Nodes = [
+  {
+    name: process.env.LAVALINK_NAME,
+    url: process.env.LAVALINK_URI,
+    auth: process.env.LAVALINK_PASSWORD,
+  },
+];
 
 class MusicBot {
   client;
+  shoukaku;
 
   constructor() {
     this.client = new Client({
@@ -12,6 +22,12 @@ class MusicBot {
         GatewayIntentBits.GuildMessages,
       ],
     });
+
+    this.shoukaku = new Shoukaku(new Connectors.DiscordJS(this.client), Nodes);
+    this.shoukaku.on("error", (_, error) => console.error(error));
+    this.shoukaku.once("ready", (name) =>
+      console.log(`Shoukaku is Ready! ${name}`),
+    );
 
     this.client.on(Events.ClientReady, () =>
       console.log(`Logged in as ${this.client.user.tag}!`),
